eabb10202b131e05d9c00da3a05204e9d8779e2b
kirillovcinnikov <kirock.98@mail.ru>
2021-10-10 09:07:19 +0300

    added client

---
+++ src/test/java/ru/vtb/moretech/DatahubClientTest.java
@@ -0,0 +1,19 @@
+package ru.vtb.moretech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.vtb.moretech.usecase.DataHubUseCase;
+
+@SpringBootTest
+public class DatahubClientTest {
+
+    @Autowired
+    DataHubUseCase useCase;
+
+    @Test
+    public void dataHub() {
+        System.out.println(useCase.getData());
+    }
+
+}
